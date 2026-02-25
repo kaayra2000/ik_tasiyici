51e8a7285264a84ea4d56a06b9eb73fdf50d7699
Muhammed Kayra Bulut <kayrabulut39@gmail.com>
2026-02-25 14:17:32 +0300

    yanlışlıkla eklenen dosyalar kaldırıldı

--- inspect2.py
+++
@@ -1,7 +0,0 @@
-import openpyxl
-
-wb = openpyxl.load_workbook("docs/EN YENİ DK format1 v2.xlsx")
-ws = wb.active
-
-for col in range(1, 15):
-    print(f"Col {col}: {ws.cell(row=10, column=col).value}")
